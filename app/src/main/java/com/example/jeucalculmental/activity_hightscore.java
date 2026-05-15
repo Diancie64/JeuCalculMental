0bba1a74861dff40fc87345a97c72f7f76815908
Alison Pierre-Louis <pierre.louisalison2006@gmail.com>
2026-05-15 17:40:32 +0200

    Ajout de la page des scores

--- app/src/main/java/com/example/jeucalculmental/activity_hightscore.java
+++ app/src/main/java/com/example/jeucalculmental/activity_hightscore.java
@@ -1,9 +1,6 @@
 package com.example.jeucalculmental;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button buttonHightScore;
+public class activity_hightscore extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hightscore);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        buttonHightScore = findViewById(R.id.button_hight_score);
-        buttonHightScore.setOnClickListener( v -> {
-            Intent intent = new Intent(this, activity_hightscore.class);
-            startActivity(intent);
-            Toast.makeText(this, "hight scores", Toast.LENGTH_LONG).show();
-
         });
     }
 }
